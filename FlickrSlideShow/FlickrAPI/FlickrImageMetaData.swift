5189893ab69f0e177b32637a0672bd7d8cc70adf
0xF0D0 <goodbumsu@gmail.com>
2018-04-28 18:33:11 +0900

    Fix Indentation
    Signed-off-by: 0xF0D0 <goodbumsu@gmail.com>

--- FlickrSlideShow/FlickrAPI/FlickrImageMetaData.swift
+++ FlickrSlideShow/FlickrAPI/FlickrImageMetaData.swift
@@ -9,47 +9,47 @@
 import Foundation
 
 struct FlickrImageMetaData {
-    let title: String
-    let flickrLink: String
-    let mediaLink: String
+  let title: String
+  let flickrLink: String
+  let mediaLink: String
 }
 
 extension FlickrImageMetaData: Decodable {
-    private enum CodingKeys: CodingKey {
-        case title
-        case link
-        case media
-    }
+  private enum CodingKeys: CodingKey {
+    case title
+    case link
+    case media
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
     
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.title = try values.decode(String.self, forKey: .title)
-        self.flickrLink = try values.decode(String.self, forKey: .link)
-        self.mediaLink = (try values.decode(FlickrMediaResponse.self, forKey: .media)).mediaLink
-    }
+    self.title = try values.decode(String.self, forKey: .title)
+    self.flickrLink = try values.decode(String.self, forKey: .link)
+    self.mediaLink = (try values.decode(FlickrMediaResponse.self, forKey: .media)).mediaLink
+  }
 }
 
 /**
  This struct is only used for parsing media link from FlickrListResponse(string below).
-    ```
-    ...
-    "media": {"m":"https:\/\/farm1.staticflickr.com\/908\/26884732907_42175628a2_m.jpg"}
-    ...
-    ```
+ ```
+ ...
+ "media": {"m":"https:\/\/farm1.staticflickr.com\/908\/26884732907_42175628a2_m.jpg"}
+ ...
+ ```
  */
 private struct FlickrMediaResponse {
-    let mediaLink: String
+  let mediaLink: String
 }
 
 extension FlickrMediaResponse: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case image = "m"
-    }
+  private enum CodingKeys: String, CodingKey {
+    case image = "m"
+  }
+  
+  public init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
     
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.mediaLink = try values.decode(String.self, forKey: .image)
-    }
+    self.mediaLink = try values.decode(String.self, forKey: .image)
+  }
 }
