545ab3ddf914edc8f2176640f8351dd69194c132
0xF0D0 <goodbumsu@gmail.com>
2018-04-28 23:02:37 +0900

    Image fade in \& fade out animation
    Signed-off-by: 0xF0D0 <goodbumsu@gmail.com>

--- FlickrSlideShow/ViewControllers/SlideViewController.swift
+++ FlickrSlideShow/ViewControllers/SlideViewController.swift
@@ -12,11 +12,25 @@ class SlideViewController: UIViewController {
   
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+  ///Seconds that image has been exposed
+  var exposedSeconds = 0
   
+  ///Duration of slide show
+  var duration = 4
+  
+  var timer: Timer?
+  
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                 target: self,
+                                 selector: #selector(checkIfTimeHasExceeded),
+                                 userInfo: nil, repeats: true)
   }
   
   override func didReceiveMemoryWarning() {
@@ -25,4 +39,45 @@ class SlideViewController: UIViewController {
   }
   
   
+  @IBAction func valueChanged(_ sender: UISlider) {
+    let sliderValue = lroundf(sender.value)
+    
+    durationLabel.text = "duration: \(sliderValue+1)"
+    duration = sliderValue+1
+    
+    sender.setValue(Float(sliderValue), animated: false)
+  }
+  
+}
+
+extension SlideViewController {
+  @objc private func checkIfTimeHasExceeded() {
+    exposedSeconds += 1
+    
+    if exposedSeconds >= duration {
+      timer?.invalidate()
+      animateToNextImage()
+    }
+  }
+  
+  private func animateToNextImage() {
+    exposedSeconds = 0
+    
+    UIView.animate(withDuration: 0.5, animations: {
+      self.imageView.alpha = 0
+    }) { finished in
+      if finished {
+        //change image
+        self.imageView.backgroundColor = (self.imageView.backgroundColor ?? UIColor.red) == UIColor.red ?  UIColor.blue : UIColor.red
+        UIView.animate(withDuration: 0.5, animations: {
+          self.imageView.alpha = 1
+        }, completion: { (finished) in
+          self.timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                       target: self,
+                                       selector: #selector(self.checkIfTimeHasExceeded),
+                                       userInfo: nil, repeats: true)
+        })
+      }
+    }
+  }
 }
