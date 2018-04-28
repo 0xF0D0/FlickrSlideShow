1590d877db72be4c2b778bfe5fd9f817e09ce653
0xF0D0 <goodbumsu@gmail.com>
2018-04-28 10:47:53 +0900

    Initial Project
    Signed-off-by: 0xF0D0 <goodbumsu@gmail.com>

---
+++ FlickrSlideShow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrSlideShow
+//
+//  Created by 김범수 on 2018. 4. 28..
+//  Copyright © 2018년 bs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
